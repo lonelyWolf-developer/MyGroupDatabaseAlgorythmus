7985b47b7140725360e1397ff6f8da599f733407
Vlk samotář <spravce@vlksamotar.cz>
2023-03-14 15:27:01 +0100

    Add project files.

---
+++ MyGroupDatabaseAlgorythmus/Comment.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyGroupDatabaseAlgorythmus
+{
+	public class Comment
+	{
+		public Comment(int commentId, int? parrentCommentId, int sent)
+		{
+			CommentId = commentId;
+			ParrentCommentId = parrentCommentId;
+			Sent = sent;
+		}
+
+		public int CommentId {  get; set; }
+
+		public int? ParrentCommentId { get; set; }
+
+		public int Sent { get; set; }
+	}
+}
---
+++ MyGroupDatabaseAlgorythmus/Program.cs
@@ -0,0 +1,52 @@
+﻿
+using MyGroupDatabaseAlgorythmus;
+
+List<Comment> comments = new List<Comment>
+{
+	new Comment( 1, null, 8),
+	new Comment( 2, null, 9),
+	new Comment( 3, 2, 10),
+	new Comment( 4, 3, 11),
+	new Comment( 5, 3, 12),
+	new Comment( 6, null, 13),
+	new Comment( 7, 6, 14),
+	new Comment( 8, 4, 15),
+	new Comment( 9, 5, 16),
+	new Comment( 10, 11, 17),
+	new Comment( 11, null, 18)
+};
+
+List<Comment> queryWithoutParrent = comments.Where(c => c.ParrentCommentId == null).OrderByDescending(c => c.Sent).ToList();
+
+foreach (Comment comment in queryWithoutParrent)
+{
+    Console.WriteLine(comment.CommentId);
+}
+
+Console.WriteLine();
+
+var queryWithParrent = from c in comments
+					   where c.ParrentCommentId != null
+					   orderby c.Sent descending
+					   group c by c.ParrentCommentId;
+
+var queryVariable = queryWithParrent.ToList();
+
+List<Comment> endsCollection = queryWithoutParrent;
+
+for (int i = 0; i < queryWithoutParrent.Count; i++)
+{
+	for(int j = 0; j < queryVariable.Count; j++)
+	{
+		if (queryWithoutParrent[i].CommentId == queryVariable[j].First().ParrentCommentId)
+		{
+			endsCollection.InsertRange(i + 1, queryVariable[j]);
+			queryVariable.Remove(queryVariable[j]);
+		}
+	}	
+}
+
+foreach(Comment comment in endsCollection)
+    Console.WriteLine(comment.CommentId);
+
+Console.ReadKey();
